8123c646d1125569c7f411b72b560002a8e1f820
Larvouu <ghiassiantoine@gmail.com>
2021-03-22 14:16:17 +0100

    Ajout script

---
+++ corriger_folder_path_xml.py
@@ -0,0 +1,56 @@
+#Avant d'utiliser ce code, on admet que le tag <filename> est correct ! 
+
+#importing os
+import os
+import xml.dom.minidom as md
+from pathlib import Path
+
+folderPath       = 'images_et_xml\\'                             # mettre le path 
+
+suffix_xml       = '.xml'
+suffix_xml_upper = '.XML'
+
+# Function to modify XML file tag <name> xxx </name>
+def modifyFolderAndPathInXml(xml_file) :
+
+    file = md.parse(xml_file) 
+
+    # modifying the value of tag <folder>
+    file.getElementsByTagName("folder")[ 0 ].firstChild.nodeValue = folderPath
+    # modifying the value of tag <path>
+    file.getElementsByTagName("path")[ 0 ].firstChild.nodeValue = Path(xml_file).absolute()
+
+    # writing the changes in "file" object to  
+    # the "xml_file.xml" file 
+    with open(xml_file, "w" ) as fs:  
+    
+        fs.write( file.toxml() ) 
+        fs.close()  
+
+    #print success
+    print("XML mis à jour")
+    
+
+def correctionEnMasse() :
+
+    for count, filename in enumerate(os.listdir(folderPath)): 
+
+        if filename.endswith(suffix_xml) or filename.endswith(suffix_xml_upper) :
+
+            modifyFolderAndPathInXml(folderPath + filename)
+
+  
+# Function to rename multiple files in a folder
+def main(): 
+    correctionEnMasse()
+    print("____________________________________________________________________")
+    print("fichier(s) mis à jour.")
+    print("____________________________________________________________________")
+    
+    
+  
+# Driver Code 
+if __name__ == '__main__': 
+      
+    # Calling main() function 
+    main() 
