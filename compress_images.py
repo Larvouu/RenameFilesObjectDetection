55ab74d86cd2c84696e71671d78aceb8bbd99ce0
Larvouu <ghiassiantoine@gmail.com>
2021-03-24 11:57:48 +0100

    ajout format jpeg

--- compress_images.py
+++ compress_images.py
@@ -7,10 +7,12 @@ from pathlib import Path
 folderPath       = 'bonjour_changez_moi_mis_laissez_les_slash\\'      # mettre le path 
 image_quality    = 20                        # la qualité de la compression, 100 : pas de compression | 20 : grosse compression
 
-suffix_jpg       = '.jpg'
-suffix_jpg_upper = '.JPG'
-suffix_xml       = '.xml'
-suffix_xml_upper = '.XML'
+suffix_jpg        = '.jpg'
+suffix_jpg_upper  = '.JPG'
+suffix_jpeg       = '.jpeg'
+suffix_jpeg_upper = '.JPEG'
+suffix_xml        = '.xml'
+suffix_xml_upper  = '.XML'
 
 # Function to compress images
 def compressImage(image) :
@@ -23,7 +25,7 @@ def compressAllInFolder() :
 
     for count, filename in enumerate(os.listdir(folderPath)): 
 
-        if filename.endswith(suffix_jpg) or filename.endswith(suffix_jpg_upper) :
+        if filename.endswith(suffix_jpg) or filename.endswith(suffix_jpg_upper) or filename.endswith(suffix_jpeg) or filename.endswith(suffix_jpeg_upper) :
 
             compressImage(folderPath + filename)
 
